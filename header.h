90f423b0e12a9d987924cc954e09c3eb6262faf5
Mihael Kolembus <84499341+Kole01@users.noreply.github.com>
2021-05-27 10:33:15 +0200

    Update header.h

--- header.h
+++ header.h
@@ -2,8 +2,10 @@
 #define HEADER_H
 
 void menu();
-void difficulty();
-void game8x8();
-void game16x16();
+void boardDifficulty();
+void boardGenerate();
+void boardGuess();
+void boardPrint();
+
 
 #endif
