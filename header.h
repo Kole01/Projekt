864cade2905c93d016718b16eafcc4d91d961b0c
Mihael Kolembus <84499341+Kole01@users.noreply.github.com>
2021-06-06 17:32:30 +0200

    Update header.h

--- header.h
+++ header.h
@@ -1,6 +1,14 @@
 #ifndef HEADER_H
 #define HEADER_H
 
+typedef struct player {
+	int id;
+	char username[20];
+	int score;
+	float time;
+
+}PLAYER;
+
 void menu();
 void boardDifficulty();
 void boardGenerate();
@@ -12,5 +20,16 @@ void finalBoard();
 void endGame();
 void boardMine();
 void win();
+void writeFile();
+void fileOpening();
+void outputFile();
+void menuScores();
+void loadFile();
+void deleteScores();
+void deleteSpecificScore();
+void sort();
+void change();
+
+
 
 #endif
